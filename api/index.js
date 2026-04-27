f053a4f271e4c65d2fa90e165079a955ec588c8c
Jahanzaib Piracha <zaibjahan.200@gmail.com>
2026-04-27 21:41:26 +0500

    Use direct Express handler for Vercel catch-all route

--- api/index.js
+++ api/index.js
@@ -1,10 +1,8 @@
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '..', 'backend', '.env') });
 
-const serverless = require('serverless-http');
 const { app, prepare } = require('../backend/src/app');
 
-let handler;
 let preparePromise;
 
 function ensurePrepared() {
@@ -15,16 +13,12 @@ function ensurePrepared() {
   }
 }
 
-function getHandler() {
-  if (!handler) {
-    handler = serverless(app);
-    ensurePrepared();
+module.exports = async (req, res) => {
+  ensurePrepared();
+
+  if (typeof req.url === 'string' && !req.url.startsWith('/api')) {
+    req.url = `/api${req.url.startsWith('/') ? '' : '/'}${req.url}`;
   }
 
-  return handler;
-}
-
-module.exports = async (req, res) => {
-  const currentHandler = getHandler();
-  return currentHandler(req, res);
+  return app(req, res);
 };
